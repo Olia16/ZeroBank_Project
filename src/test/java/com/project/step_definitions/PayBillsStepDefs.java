4cab5255b7d54cf744e2556d7a700642f461e96c
Malik <aamiramallick7@gmail.com>
2022-01-20 16:56:26 -0500

    Worked on PayBillsStepDefs

--- src/test/java/com/project/step_definitions/PayBillsStepDefs.java
+++ src/test/java/com/project/step_definitions/PayBillsStepDefs.java
@@ -1,45 +1,40 @@
 package com.project.step_definitions;
 
+import com.project.pages.PayBillsPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class PayBillsStepDefs {
 
-    @Given("User is on the pay bills page")
-    public void user_is_on_the_pay_bills_page() {
+    PayBillsPage payBillsPage = new PayBillsPage();
 
-    }
-    @Given("the {string} title is displayed")
-    public void the_title_is_displayed(String string) {
 
-    }
-    @When("User click on payee box and select {string}")
-    public void user_click_on_payee_box_and_select(String string) {
-
-    }
-    @When("User clicks on account box and selects {string}")
-    public void user_clicks_on_account_box_and_selects(String string) {
-
-    }
     @When("User clicks on amount box and enters {string}")
-    public void user_clicks_on_amount_box_and_enters(String string) {
-
+    public void user_clicks_on_amount_box_and_enters(String amount) {
+       payBillsPage.accountBox.sendKeys(amount);
     }
+
     @When("User click on date box and enters {string}")
-    public void user_click_on_date_box_and_enters(String string) {
+    public void user_click_on_date_box_and_enters(String todayDate) {
+        payBillsPage.dateBox.sendKeys(todayDate);
 
     }
+
     @When("User clicks on description box and enters {string}")
-    public void user_clicks_on_description_box_and_enters(String string) {
-
+    public void user_clicks_on_description_box_and_enters(String desc) {
+        payBillsPage.descriptionBox.sendKeys(desc);
     }
+
     @When("User clicks on Pay button")
     public void user_clicks_on_pay_button() {
-
+        payBillsPage.payButton.click();
     }
-    @Then("{string} should be displayed")
-    public void should_be_displayed(String string) {
 
+    @Then("{string} should be displayed")
+    public void should_be_displayed(String expectedMsg) {
+        String actualMsg = payBillsPage.successMsg.getText();
+        Assert.assertEquals("Message doesn't match",expectedMsg,actualMsg);
     }
 }
