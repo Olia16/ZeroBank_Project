7f9999289208f2bc2152aa3e4884ec3f5f362a29
Elle323 <ellieamineva@gmail.com>
2022-01-20 17:14:28 -0500

    updated PayBillsStepDefs

--- src/test/java/com/project/step_definitions/PayBillsStepDefs.java
+++ src/test/java/com/project/step_definitions/PayBillsStepDefs.java
@@ -1,19 +1,41 @@
 package com.project.step_definitions;
 
 import com.project.pages.PayBillsPage;
+import com.project.utils.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 public class PayBillsStepDefs {
 
     PayBillsPage payBillsPage = new PayBillsPage();
 
+    @Given("User is on the pay bills page")
+    public void user_is_on_the_pay_bills_page() {
+        Driver.getDriver().findElement(By.linkText("Pay Bills")).click();
+    }
+    @Given("the {string} title is displayed")
+    public void the_title_is_displayed(String expectedTitle) {
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals("Title does NOT match", expectedTitle, actualTitle);
+    }
+    @When("User click on payee box and select {string}")
+    public void user_click_on_payee_box_and_select(String payeeOption) {
+        Select select = new Select(payBillsPage.payeeBox);
+        select.selectByVisibleText(payeeOption);
+    }
+    @When("User clicks on account box and selects {string}")
+    public void user_clicks_on_account_box_and_selects(String accountOption) {
+        Select select = new Select(payBillsPage.accountBox);
+        select.selectByVisibleText(accountOption);
+    }
 
     @When("User clicks on amount box and enters {string}")
     public void user_clicks_on_amount_box_and_enters(String amount) {
-       payBillsPage.accountBox.sendKeys(amount);
+        payBillsPage.accountBox.sendKeys(amount);
     }
 
     @When("User click on date box and enters {string}")
@@ -37,4 +59,5 @@ public class PayBillsStepDefs {
         String actualMsg = payBillsPage.successMsg.getText();
         Assert.assertEquals("Message doesn't match",expectedMsg,actualMsg);
     }
+
 }
