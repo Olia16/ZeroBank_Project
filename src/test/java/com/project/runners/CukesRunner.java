37ab6d07e12fdfeb2d9f450ad4b9372fe5bff51f
olhakobiuk <oliakobjuk0952@gmail.com>
2022-01-20 23:44:40 -0500

    hooks added

--- src/test/java/com/project/runners/CukesRunner.java
+++ src/test/java/com/project/runners/CukesRunner.java
@@ -14,7 +14,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/project/step_definitions",
         dryRun = false,
-        tags = "@paybills"
+        tags = ""
 )
 public class CukesRunner {
+
 }
