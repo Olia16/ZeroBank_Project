23c49efa8854431b0a663920f93fa17ec281c0a9
gvnburak <gvnburak@gmail.com>
2022-01-31 12:14:27 -0500

    Purchase Foreign Currency feature file added. StepDefs added. WebElements added to PayBillsPage.

--- src/test/java/com/project/pages/PayBillsPage.java
+++ src/test/java/com/project/pages/PayBillsPage.java
@@ -1,9 +1,14 @@
 package com.project.pages;
 
+import com.project.utils.BrowserUtils;
 import com.project.utils.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Set;
 
 public class PayBillsPage {
 
@@ -31,6 +36,18 @@ public class PayBillsPage {
 
     @FindBy(xpath = "//span[@title='$ 1000 payed to payee bofa']")
     public WebElement successMsg;
+
+    @FindBy(id = "pc_currency")
+    public static WebElement currencyList;
+
+    @FindBy(id = "user_login")
+    public  WebElement userNameField;
+
+    @FindBy(id = "user_password")
+    public static WebElement passwordField;
+
+
+
 
 
 }
