cd7fad4cbffa97563589db6ba278b3e76ddec016
Elle323 <ellieamineva@gmail.com>
2022-02-01 14:35:17 -0500

    added Scenario #2,3,4 to FindTransactions feature

--- src/test/java/com/project/pages/FindTransactionsTabPage.java
+++ src/test/java/com/project/pages/FindTransactionsTabPage.java
@@ -1,9 +1,13 @@
 package com.project.pages;
 
 import com.project.utils.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+import java.util.Locale;
 
 public class FindTransactionsTabPage {
 
@@ -21,6 +25,26 @@ public class FindTransactionsTabPage {
 
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement findBtn;
+
+    @FindBy(id = "aa_description")
+    public WebElement descriptionBox;
+
+    @FindBy(id = "aa_type")
+    public WebElement typeDropdown;
+
+    public List<WebElement> getInputFromResultsTable(String input){
+        return Driver.getDriver().findElements(By.xpath("//div[@id='filtered_transactions_for_account']//td[contains(text(),'"+input+"')]"));
+    }
+
+    public List<WebElement> getColumn(String type){
+        int index = 0;
+        if(type.equals("Deposit")){
+            index = 3;
+        }else if(type.equals("Withdrawal")){
+            index = 4;
+        }
+        return Driver.getDriver().findElements(By.xpath("//div[@id='filtered_transactions_for_account']//td["+index+"]"));
+    }
 
 
 
--- src/test/java/com/project/step_definitions/FindTransactionsStepDefs.java
+++ src/test/java/com/project/step_definitions/FindTransactionsStepDefs.java
@@ -1,13 +1,16 @@
 package com.project.step_definitions;
 
 import com.project.pages.FindTransactionsTabPage;
+import com.project.utils.BrowserUtils;
+import io.cucumber.java.en.But;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import javafx.scene.input.DataFormat;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
-import java.time.LocalDate;
+import java.util.List;
 
 public class FindTransactionsStepDefs {
 
@@ -42,5 +45,76 @@ public class FindTransactionsStepDefs {
 
     }
 
+    String description = "";
+
+    @When("the user enters description {string}")
+    public void theUserEntersDescription(String input) {
+        description = input.toUpperCase();
+        transactionsTabPage.descriptionBox.clear();
+        transactionsTabPage.descriptionBox.sendKeys(description);
+    }
+
+    @Then("results table should only show descriptions containing {string}")
+    public void resultsTableShouldOnlyShowDescriptionsContaining(String correctDescription) {
+        List<WebElement> actualOutputFromRT = transactionsTabPage.getInputFromResultsTable(description);
+        for(WebElement each : actualOutputFromRT){
+            String actualOutput = each.getText();
+            Assert.assertTrue(actualOutput.contains(correctDescription));
+            System.out.println(each.getText());
+        }
+    }
+
+    @But("results table should not show descriptions containing {string}")
+    public void resultsTableShouldNotShowDescriptionsContaining(String wrongDescription) {
+        List<WebElement> actualOutputFromRT = transactionsTabPage.getInputFromResultsTable(description);
+        for(WebElement each : actualOutputFromRT){
+            String actualOutput = each.getText();
+            Assert.assertFalse(actualOutput.contains(wrongDescription));
+            System.out.println(each.getText());
+        }
+    }
+
+    @Then("results table should show at least one result under {string} and {string}")
+    public void results_table_should_show_at_least_one_result_under_and(String type, String otherType) {
+        int count = 0;
+        for(WebElement each : transactionsTabPage.getColumn(type)){
+            if(!each.getText().isEmpty()){
+                count++;
+            }
+        }
+        Assert.assertTrue(count>0);
+        count = 0;
+        for(WebElement each : transactionsTabPage.getColumn(otherType)){
+            if(!each.getText().isEmpty()){
+                count++;
+            }
+        }
+        Assert.assertTrue(count>0);
+    }
+
+    @When("user selects type {string}")
+    public void user_selects_type(String type) {
+        BrowserUtils.sleep(3);
+        Select select = new Select(transactionsTabPage.typeDropdown);
+        select.selectByVisibleText(type);
+    }
+
+    @Then("results table should show at least one result under {string}")
+    public void results_table_should_show_at_least_one_result_under(String type) {
+        int count = 0;
+        for(WebElement each : transactionsTabPage.getColumn(type)){
+            if(!each.getText().isEmpty()){
+                count++;
+            }
+        }
+        Assert.assertTrue(count>0);
+    }
+
+    @Then("results table should show no result under {string}")
+    public void results_table_should_show_no_result_under(String otherType) {
+        for(WebElement each : transactionsTabPage.getColumn(otherType)){
+            Assert.assertEquals("",each.getText());
+        }
+    }
 
 }
